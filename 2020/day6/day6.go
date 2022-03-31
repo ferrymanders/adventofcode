2c1b8e8ecd8a78d5f363b8dc539f71cb849c9f4b
Ferry Manders <ferrymanders@users.noreply.github.com>
2022-03-31 14:36:24 +0200

    added day6

---
+++ 2020/day6/day6.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "regexp"
+    //"sort"
+    //"strings"
+    //"strconv"
+)
+
+
+func main() {
+    questionMap := make(map[string]int)
+    part1 := int(0)
+    part2 := int(0)
+    members := int(0)
+
+    file, err := os.Open("input")
+    if err != nil {
+        fmt.Println("err Open")
+    }
+
+    // Collect data per customs declaration group
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        // New group starts on empty newline
+        matched, _ := regexp.MatchString(`^\s*$`, line)
+        if matched {
+            for key, value := range questionMap {
+                part1++
+                if ( value == members ) {
+                    part2++
+                }
+                delete(questionMap, key)
+            }
+            members = int(0)
+        } else {
+            for _, char := range line {
+                questionMap[string(char)]++
+            }
+            members++
+        }
+
+    }
+
+    // Part 1
+    fmt.Println("Part1: ", part1)
+    // Part 2
+    fmt.Println("Part2: ", part2)
+
+}
