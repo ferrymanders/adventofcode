0f75079dc1c191f32b4c1e226c118523576fe54a
Ferry Manders <github@blackring.net>
2024-12-03 08:41:40 +0100

    day3 - part2

--- 2024/day3/day3.sh
+++ 2024/day3/day3.sh
@@ -3,17 +3,27 @@
 INPUT="input"
 
 PART1=0
+PART2=0
+ENABLED=true
 
-REGEX="mul\([0-9]+,[0-9]+\)"
+REGEX="(do(|n't)\(\)|mul\([0-9]+,[0-9]+\))"
 
 
-for MUL in $(grep -Eo "$REGEX" $INPUT);
+for COMMAND in $(grep -Eo "$REGEX" $INPUT);
 do
-    # Cleanup
-    MUL=$(echo $MUL | sed -e 's/mul(//' -e 's/)//')
-    IFS=',' read FIRST SECOND <<<`echo $MUL`
+    [[ $COMMAND == "do()" ]] && ENABLED=true
+    [[ $COMMAND == "don't()" ]] && ENABLED=false
 
-    PART1=$(( PART1 + $((FIRST * SECOND)) ))
+    CALC=$(echo $COMMAND | sed -e 's/mul(//' -e 's/)//')
+    IFS=',' read FIRST SECOND <<<`echo $CALC`
+
+    if [[ $COMMAND == mul* && $ENABLED == true ]];
+    then 
+        PART2=$(( PART2 + $((FIRST * SECOND)) ))
+    fi
+
+    [[ $COMMAND == mul* ]] && PART1=$(( PART1 + $((FIRST * SECOND)) ))
 done
 
 echo "# Part1: $PART1"
+echo "# Part2: $PART2"
